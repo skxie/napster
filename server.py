e99bf0a4a9789f62338087ca3bb0bcf27e2e84b3
skxie <shuk.xie@gmail.com>
2012-10-15 22:12:04 -0500

    changed the welcome

--- server.py
+++ server.py
@@ -84,15 +84,9 @@ class CentralServer:
 		return fileList
 
 	def welcome(self):
-		#return '''
-		#Hi, this a p2p system which provides the following functions:
-		#1. getFullFileList. get the names of full shared files.
-		#2. search. search the specific file to check whether it is shared in this system.
-		#3. downlad. download a particular file from a specific peer.
-		#'''
 		return '''
 		Hi, this a p2p system which provides the following functions:
-		1. getFullFileList. obtain the full list of the shared files in this system.
+		1. getFullFileList. obtain the full list of shared files in this system.
 		2. search. search the specific file to check whether it is shared in this system.
 		3. downlad. download a particular file from a specific peer.
 		'''
